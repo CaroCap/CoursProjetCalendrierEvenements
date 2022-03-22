0213bbae2ced344153cbc31cd36e856ed745e12c
CaroCap <carolinecap.event@gmail.com>
2022-03-22 10:40:42 +0100

    login auth

--- migrations/Version20220322085250.php
+++ migrations/Version20220322085250.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220322084214 extends AbstractMigration
+final class Version20220322085250 extends AbstractMigration
 {
     public function getDescription(): string
     {
